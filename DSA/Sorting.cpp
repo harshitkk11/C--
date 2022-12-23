4d9b467b0205acff7e0aa1e228726a46d053f6df
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2022-12-23 15:33:13 +0530

    new commit

--- DSA/Sorting.cpp
+++ DSA/Sorting.cpp
@@ -9,23 +9,19 @@ void print(vector<int>A){
 }
 
 void sort(vector<int>&A){
-    for(int j=0; j<A.size()-1; j++){
-        for(int i=1; i<A.size(); i++){
-
-            if(A[j]>A[i]){
-                int temp = A[j];
-                A[j] = A[i];
-                A[i] = A[j];
+    for(int i=0; i<A.size()-1; i++){
+        int min_indx = i;
+        for(int j=1;j<A.size(); j++){
+            if(A[min_indx]>A[j]){
+                min_indx = j;
             }
-            
         }
-        print(A);
     }
 }
 
 int main(){
     vector<int>A = {5,2,4,6,3,5,1,3,6,4,7};
-    print(A);
+    // print(A);
 
     // sort(A);
 
