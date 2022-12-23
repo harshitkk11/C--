9d187db3a1e0982d1d303f6419b1bf2bc840cca0
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2022-12-23 16:01:01 +0530

    new commit

--- DSA/Codeforce/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-int first = 0, last = A.size()-1;
-
-    // while (first<=last){
-    //     int mid = first + (last - first)/2;
-    //     if(target>A[mid]){
-    //         first = mid+1;
-    //     }
-    //     else if(target<)
-    // }
--- DSA/Sorting.cpp
+++ DSA/Sorting.cpp
@@ -21,7 +21,7 @@ void sort(vector<int>&A){
 
 int main(){
     vector<int>A = {5,2,4,6,3,5,1,3,6,4,7};
-    // print(A);
+    print(A);
 
     // sort(A);
 
