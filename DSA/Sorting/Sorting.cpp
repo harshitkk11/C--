72b7bd44cd16e1b87efc2f1d39f0f2d1f30a719a
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2022-12-31 01:01:08 +0530

    new commit

--- DSA/Sorting/Sorting.cpp
+++ DSA/Sorting/Sorting.cpp
@@ -7,9 +7,13 @@ void print(vector<int>A){
     {
         cout<<A[i]<<" ";
     }
+    cout<<endl;
 }
 
 void Selection_Sort(vector<int>A){
+    // Time complexity = O(n)*O(n) = O(n^2)
+    // O(n) for first loop to select elements one by one
+    // O(n) for second loop to compare elements with each other
     for (int i=0; i<A.size(); i++){
         for(int j=i+1; j<A.size();j++){
             int temp;
@@ -23,13 +27,27 @@ void Selection_Sort(vector<int>A){
     print(A);
 }
 
-void Bubble_Sort(vector<int>A){
+void Bubble_Sort(vector<int>A, int n){
+    if(n == 0){
+        print(A);
+        return;
+    }
 
+    for(int i=0; i<A.size()-1; i++){
+        if (A[i] > A[i+1]){
+            int temp = A[i];
+            A[i] = A[i+1];
+            A[i+1] = temp;
+        }
+    }
+    Bubble_Sort(A, n-1);
 }
 
 int main(){
-    vector<int>A = {5,4,8,10,0};
+    vector<int>A = {5,4,8,10,1};
+
     Selection_Sort(A);
+    Bubble_Sort(A, A.size());
 
     return 0;
 }
