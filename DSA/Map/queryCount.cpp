dab00165dad5415ab250ed8bab458fc70d12a73c
harshitkumar11 <harshitkumar8179@gmail.com>
2023-01-23 12:26:59 +0530

    new commit

---
+++ DSA/Map/queryCount.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void searchquery(vector<int>A){
+    unordered_map<string, int> wordfrq;
+
+    for (int i = 0; i < A.size(); i++) wordfrq[A[i]]++;
+    
+    
+
+}
+
+int main(){
+
+    return 0;
+}
