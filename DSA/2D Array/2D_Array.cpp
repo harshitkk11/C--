9223581506ad3e3cf65543bdbfe2f01b2ff3f817
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2023-02-03 23:03:22 +0530

    new commit

--- DSA/2D Array/2D_Array.cpp
+++ DSA/2D Array/2D_Array.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int mamin(){
-    
+void print(vector<vector<int>> arr){
+    for(int i=0; i<arr.size(); i++){
+        for(int j=0; j<arr.size(); j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    vector<vector<int>> arr = {{1,2,3,4}, {2,4,6,8}, {3,5,7,9}};;
+    print(arr);
     return 0;
 }
