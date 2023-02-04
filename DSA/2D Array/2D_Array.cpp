752c17df74d9e38db9fed8e527c50080409e37ac
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2023-02-04 23:59:09 +0530

    new commit

--- DSA/2D Array/2D_Array.cpp
+++ DSA/2D Array/2D_Array.cpp
@@ -2,17 +2,26 @@
 #include<vector>
 using namespace std;
 
+// void diagonals(vector<vector<int>> &arr){
+//     for(int i=0; i<arr.size(); i++){
+//         for(int j=0; j<arr[i].size(); j++){
+//             if(i = j){
+//                 cout<<arr[i][j]<<" ";
+//             }
+//         }
+//     }
+// }
+
 void print(vector<vector<int>> arr){
-    for(int i=0; i<arr.size(); i++){
-        for(int j=0; j<arr.size(); j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
+    int k=arr.size()-1;
+    for(int i=0; i<arr[i].size(); i++){
+        cout<<arr[i][k--]<<" ";
     }
 }
 
 int main(){
     vector<vector<int>> arr = {{1,2,3,4}, {2,4,6,8}, {3,5,7,9}};;
     print(arr);
+    // diagonals(arr);
     return 0;
 }
