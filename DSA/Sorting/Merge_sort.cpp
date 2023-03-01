532138ea3e6f6ae6c88ab80246bd9b816920e795
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2023-03-02 00:52:46 +0530

    new commit

--- DSA/Sorting/Merge_sort.cpp
+++ DSA/Sorting/Merge_sort.cpp
@@ -1,15 +1,67 @@
 #include<iostream>
 using namespace std;
 
-void sort(int arr[], int start){
-    if(arr[0] > arr[sizeof(arr)-1]){
-        cout<<"True"<<endl;
+void merge(int arr[], int start, int mid, int end){
+    int left_len = mid-start+1;
+    int right_len = end-mid;
+    int *leftarray = new int [left_len];
+    int *rightarray = new int[right_len];
+
+    int arr_index = start;
+    for(int i=0; i<left_len; i++){
+        leftarray[i] = arr[arr_index++];
     }
+    arr_index = mid+1;
+    for(int j=0; j<right_len; j++){
+        rightarray[j] = arr[arr_index++];
+    }
+
+    int index1 = 0;
+    int index2 = 0;
+    arr_index = start;
+
+    while(index1 < left_len && index2 < right_len){
+        if(leftarray[index1] < rightarray[index2]){
+            arr[arr_index++] = leftarray[index1++];
+        }
+        else{
+            arr[arr_index++] = rightarray[index2++];
+        }
+    }
+
+    while(index1 < left_len){
+        arr[arr_index++] = leftarray[index1++];
+    }
+
+    while(index2 < right_len){
+        arr[arr_index++] = rightarray[index2];
+    }
+
+    delete []leftarray;
+    delete []rightarray;
+}
+
+void mergesort(int arr[], int start, int end){
+    if(start >= end){
+        return;
+    }
+    int mid = end + (end - start)/2;
+    mergesort(arr, start, mid);
+    mergesort(arr, mid+1, end);
+    merge(arr, start, mid, end);
 
 }
 
 int main(){
     int arr[] = {38, 27, 43, 3, 9, 82, 10};
-    sort(arr, 0);
+    int n = 7;
+     for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    mergesort(arr, 0, n-1);
+
+    for(int j=0; j<n; j++){
+        cout<<arr[j]<<" ";
+    }
     return 0;
 }
